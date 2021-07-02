b2c9484bba17e1f9166ad3eabeb3b51e968aeb8a
Vemund Schøyen <vemund@live.com>
2021-07-02 12:52:37 +0200

    Adds evaluation metrics

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,21 +1,27 @@
 """
 Implements evaluation metrics
 """
-import numpy
-import torch
+import numpy as np
+import torch 
 
+def tonumpy(x):
+	if isinstance(x, torch.Tensor):
+		x = x.numpy()
+	return x
 
 def rmse(y_true, y_pred):
 	"""Root mean squared error"""
-	if isinstance(y_pred, torch.Tensor):
-		y_pred = y_pred.numpy()
-
+	y_true, y_pred = map(tonumpy, (y_true, y_pred))
 	return np.sqrt(np.mean((y_true - y_pred)**2))
 
-def picp():
+def picp(Lpi, Upi, y_true):
 	"""Prediction interval coverage probability"""
-	pass
+	Lpi, Upi, y_true = map(tonumpy, (Lpi, Upi, y_true))
+	within = (Lpi < y_true).astype(int) + (Upi > y_true).astype(int)
+	within = (within == 2) # higher than lower bound, and smaller than upper bound
+	return np.mean(within)
 
-def mpiw():
+def mpiw(Lpi, Upi):
 	"""Mean prediction interval width"""
-	pass
+	Lpi, Upi = map(tonumpy, (Lpi, Upi))
+	return np.mean(Upi - Lpi)
